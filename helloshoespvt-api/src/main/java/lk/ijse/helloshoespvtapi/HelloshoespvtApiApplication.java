f8fd2e0471687ec489ed1e72986fcb11c42db215
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2024-04-20 12:52:47 +0530

    initial commit

---
+++ helloshoespvt-api/src/main/java/lk/ijse/helloshoespvtapi/HelloshoespvtApiApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.helloshoespvtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloshoespvtApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloshoespvtApiApplication.class, args);
+	}
+
+}
---
+++ helloshoespvt-api/src/test/java/lk/ijse/helloshoespvtapi/HelloshoespvtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.helloshoespvtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloshoespvtApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
