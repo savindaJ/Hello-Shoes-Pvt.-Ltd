564a8752bd1317df6bd84eff53e03100065df778
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2024-04-23 10:34:21 +0530

    add suplier view page

---
+++ helloshoespvt-frontend/controller/userSupplierController.js
@@ -0,0 +1,17 @@
+for (let i = 0; i < 10; i++) {
+    $('#tbl-customer-body').append(`
+        <tr>
+             <td class="text-center">${i}</td>
+             <td class="text-center">System Architect</td>
+             <td class="text-center">Edinburgh</td>
+             <td class="text-center">61</td>
+             <td class="text-center">2011-04-25</td>
+             <td class="text-center">$320,800</td>
+             <td class="text-center">$320,800</td>
+             <td class="text-center">$320,800</td>
+             <td class="text-center">$320,800</td>
+             <td class="text-center">$320,800</td>
+             <td class="text-center">$320,800</td>
+        </tr>
+    `)
+}
